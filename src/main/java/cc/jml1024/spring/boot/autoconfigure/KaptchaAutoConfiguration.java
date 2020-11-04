dd83dd3ffc2f4cceb099ccb0d9b4e1ceff593867
Sin Cara <jml1024@hotmail.com>
2020-11-04 18:17:41 +0800

    no message

--- src/main/java/cc/jml1024/spring/boot/autoconfigure/KaptchaAutoConfiguration.java
+++ src/main/java/cc/jml1024/spring/boot/autoconfigure/KaptchaAutoConfiguration.java
@@ -1,7 +1,67 @@
 package cc.jml1024.spring.boot.autoconfigure;
 
+import com.google.code.kaptcha.Constants;
+import com.google.code.kaptcha.Producer;
+import com.google.code.kaptcha.util.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Properties;
+
 @Configuration
-public class KaptchaAutoConfiguration {
+@EnableConfigurationProperties({KaptchaProperties.class})
+public class KaptchaAutoConfiguration implements InitializingBean {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private KaptchaProperties kaptchaProperties;
+
+    public KaptchaAutoConfiguration(KaptchaProperties kaptchaProperties) {
+        this.kaptchaProperties = kaptchaProperties;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        logger.info("nothing for checking");
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public Producer getKaptchaProducer() {
+        Config config = getCofig();
+        Producer producer = config.getProducerImpl();
+        return producer;
+    }
+
+    private Config getCofig() {
+        Properties properties = new Properties();
+        properties.put(Constants.KAPTCHA_SESSION_CONFIG_KEY, kaptchaProperties.getSessionKey());
+        properties.put(Constants.KAPTCHA_SESSION_CONFIG_DATE, kaptchaProperties.getSessionDate());
+        properties.put(Constants.KAPTCHA_BORDER, kaptchaProperties.getBorder());
+        properties.put(Constants.KAPTCHA_BORDER_COLOR, kaptchaProperties.getBorderColor());
+        properties.put(Constants.KAPTCHA_BORDER_THICKNESS, kaptchaProperties.getBorderThickness());
+        properties.put(Constants.KAPTCHA_NOISE_COLOR, kaptchaProperties.getNoiseColor());
+        properties.put(Constants.KAPTCHA_NOISE_IMPL, kaptchaProperties.getNoiseImpl());
+        properties.put(Constants.KAPTCHA_OBSCURIFICATOR_IMPL, kaptchaProperties.getObscurificatorImpl());
+        properties.put(Constants.KAPTCHA_PRODUCER_IMPL, kaptchaProperties.getProducerImpl());
+        properties.put(Constants.KAPTCHA_TEXTPRODUCER_IMPL, kaptchaProperties.getTextproducerImpl());
+        properties.put(Constants.KAPTCHA_TEXTPRODUCER_CHAR_STRING, kaptchaProperties.getTextproducerCharString());
+        properties.put(Constants.KAPTCHA_TEXTPRODUCER_CHAR_LENGTH, kaptchaProperties.getTextproducerCharLength());
+        properties.put(Constants.KAPTCHA_TEXTPRODUCER_FONT_NAMES, kaptchaProperties.getTextproducerFontNames());
+        properties.put(Constants.KAPTCHA_TEXTPRODUCER_FONT_COLOR, kaptchaProperties.getTextproducerFontColor());
+        properties.put(Constants.KAPTCHA_TEXTPRODUCER_FONT_SIZE, kaptchaProperties.getTextproducerFontSize());
+        properties.put(Constants.KAPTCHA_TEXTPRODUCER_CHAR_SPACE, kaptchaProperties.getTextproducerCharSpace());
+        properties.put(Constants.KAPTCHA_WORDRENDERER_IMPL, kaptchaProperties.getWordImpl());
+        properties.put(Constants.KAPTCHA_BACKGROUND_IMPL, kaptchaProperties.getBackgroundImpl());
+        properties.put(Constants.KAPTCHA_BACKGROUND_CLR_FROM, kaptchaProperties.getBackgroundClearFrom());
+        properties.put(Constants.KAPTCHA_BACKGROUND_CLR_TO, kaptchaProperties.getBackgroundClearTo());
+        properties.put(Constants.KAPTCHA_IMAGE_WIDTH, kaptchaProperties.getImageWidth());
+        properties.put(Constants.KAPTCHA_IMAGE_HEIGHT, kaptchaProperties.getImageHeight());
+        return new Config(properties);
+    }
 }
--- src/main/java/cc/jml1024/spring/boot/autoconfigure/KaptchaProperties.java
+++ src/main/java/cc/jml1024/spring/boot/autoconfigure/KaptchaProperties.java
@@ -2,7 +2,230 @@ package cc.jml1024.spring.boot.autoconfigure;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties("kaptcha")
+@ConfigurationProperties(prefix = "kaptcha")
 public class KaptchaProperties {
-    private String height;
+
+    private String sessionKey;
+
+    private String sessionDate;
+    /**
+     * value must be yes or no
+     */
+    private String border;
+
+    private String borderColor;
+
+    private String borderThickness;
+
+    private String noiseColor;
+
+    private String noiseImpl;
+
+    private String obscurificatorImpl;
+
+    private String producerImpl;
+
+    private String textproducerImpl;
+
+    private String textproducerCharString;
+
+    private String textproducerCharLength;
+
+    private String textproducerFontNames;
+
+    private String textproducerFontColor;
+
+    private String textproducerFontSize;
+
+    private String textproducerCharSpace;
+
+    private String wordImpl;
+
+    private String backgroundImpl;
+
+    private String backgroundClearFrom;
+
+    private String backgroundClearTo;
+
+    private String imageWidth;
+
+    private String imageHeight;
+
+
+    public String getSessionKey() {
+        return sessionKey;
+    }
+
+    public void setSessionKey(String sessionKey) {
+        this.sessionKey = sessionKey;
+    }
+
+    public String getSessionDate() {
+        return sessionDate;
+    }
+
+    public void setSessionDate(String sessionDate) {
+        this.sessionDate = sessionDate;
+    }
+
+    public String getBorder() {
+        return border;
+    }
+
+    public void setBorder(String border) {
+        this.border = border;
+    }
+
+    public String getBorderColor() {
+        return borderColor;
+    }
+
+    public void setBorderColor(String borderColor) {
+        this.borderColor = borderColor;
+    }
+
+    public String getBorderThickness() {
+        return borderThickness;
+    }
+
+    public void setBorderThickness(String borderThickness) {
+        this.borderThickness = borderThickness;
+    }
+
+    public String getNoiseColor() {
+        return noiseColor;
+    }
+
+    public void setNoiseColor(String noiseColor) {
+        this.noiseColor = noiseColor;
+    }
+
+    public String getNoiseImpl() {
+        return noiseImpl;
+    }
+
+    public void setNoiseImpl(String noiseImpl) {
+        this.noiseImpl = noiseImpl;
+    }
+
+    public String getObscurificatorImpl() {
+        return obscurificatorImpl;
+    }
+
+    public void setObscurificatorImpl(String obscurificatorImpl) {
+        this.obscurificatorImpl = obscurificatorImpl;
+    }
+
+    public String getProducerImpl() {
+        return producerImpl;
+    }
+
+    public void setProducerImpl(String producerImpl) {
+        this.producerImpl = producerImpl;
+    }
+
+    public String getTextproducerImpl() {
+        return textproducerImpl;
+    }
+
+    public void setTextproducerImpl(String textproducerImpl) {
+        this.textproducerImpl = textproducerImpl;
+    }
+
+    public String getTextproducerCharString() {
+        return textproducerCharString;
+    }
+
+    public void setTextproducerCharString(String textproducerCharString) {
+        this.textproducerCharString = textproducerCharString;
+    }
+
+    public String getTextproducerCharLength() {
+        return textproducerCharLength;
+    }
+
+    public void setTextproducerCharLength(String textproducerCharLength) {
+        this.textproducerCharLength = textproducerCharLength;
+    }
+
+    public String getTextproducerFontNames() {
+        return textproducerFontNames;
+    }
+
+    public void setTextproducerFontNames(String textproducerFontNames) {
+        this.textproducerFontNames = textproducerFontNames;
+    }
+
+    public String getTextproducerFontColor() {
+        return textproducerFontColor;
+    }
+
+    public void setTextproducerFontColor(String textproducerFontColor) {
+        this.textproducerFontColor = textproducerFontColor;
+    }
+
+    public String getTextproducerFontSize() {
+        return textproducerFontSize;
+    }
+
+    public void setTextproducerFontSize(String textproducerFontSize) {
+        this.textproducerFontSize = textproducerFontSize;
+    }
+
+    public String getTextproducerCharSpace() {
+        return textproducerCharSpace;
+    }
+
+    public void setTextproducerCharSpace(String textproducerCharSpace) {
+        this.textproducerCharSpace = textproducerCharSpace;
+    }
+
+    public String getWordImpl() {
+        return wordImpl;
+    }
+
+    public void setWordImpl(String wordImpl) {
+        this.wordImpl = wordImpl;
+    }
+
+    public String getBackgroundImpl() {
+        return backgroundImpl;
+    }
+
+    public void setBackgroundImpl(String backgroundImpl) {
+        this.backgroundImpl = backgroundImpl;
+    }
+
+    public String getBackgroundClearFrom() {
+        return backgroundClearFrom;
+    }
+
+    public void setBackgroundClearFrom(String backgroundClearFrom) {
+        this.backgroundClearFrom = backgroundClearFrom;
+    }
+
+    public String getBackgroundClearTo() {
+        return backgroundClearTo;
+    }
+
+    public void setBackgroundClearTo(String backgroundClearTo) {
+        this.backgroundClearTo = backgroundClearTo;
+    }
+
+    public String getImageWidth() {
+        return imageWidth;
+    }
+
+    public void setImageWidth(String imageWidth) {
+        this.imageWidth = imageWidth;
+    }
+
+    public String getImageHeight() {
+        return imageHeight;
+    }
+
+    public void setImageHeight(String imageHeight) {
+        this.imageHeight = imageHeight;
+    }
+
 }
